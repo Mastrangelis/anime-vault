8bfc9ea05db3ccb2e40f2a88a7e4c0d3aa7c04ad
Nikos Mastrangelis <mastragelis@outlook.com>
2023-11-27 18:33:13 +0200

    Server actions exercise

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,31 @@
+"use client";
+
+import { fetchAnime } from "@/app/action";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 function LoadMore() {
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<JSX.Element[]>([]);
+  const [page, setPage] = useState<number>(2);
+
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(page).then((result) => {
+        setData([...data, ...result]);
+        setPage((prev: number) => prev + 1);
+      });
+    }
+  }, [data, inView, page]);
+
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data}
+      </section>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
